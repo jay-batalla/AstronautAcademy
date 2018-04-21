f49d4bc92a1d37a93b2f60c3133cdffa51a245c3
Nikesh <nikytailor@gmail.com>
2018-04-20 19:24:56 -0700

    trial and error functions for location tracking, no solution yet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { HttpModule } from '@angular/http';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { Geolocation } from '@ionic-native/geolocation';
+import {LocateControl } from 'leaflet.locatecontrol';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
--- src/pages/map/map.ts
+++ src/pages/map/map.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {LocateControl } from 'leaflet.locatecontrol';
 
 /**
  * Generated class for the MapPage page.
@@ -7,6 +8,7 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
  * See https://ionicframework.com/docs/components/#navigation for more info on
  * Ionic pages and navigation.
  */
+ declare var L: any
 
 @IonicPage()
 @Component({
@@ -21,20 +23,20 @@ export class MapPage {
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad MapPage');
-
-     var map = L.Wrld.map("map", "f1e83049d9f153920ff635e55992cd9b",{ coverageTreeManifest: "https://webgl-cdn1.wrld3d.com/chunk/indoor_maps/api_requests/EIM-ad6cd210-0145-4881-878e-8f5455693527_2018_03_14_04_14_59/webgl_manifest.bin.gz", 
+    //Change coverageTreeManifest to mobile link when publishing to ios
+     var map = L.Wrld.map("map", "f1e83049d9f153920ff635e55992cd9b",{ coverageTreeManifest: "https://webgl-cdn1.wrld3d.com/chunk/indoor_maps/api_requests/EIM-ad6cd210-0145-4881-878e-8f5455693527_2018_03_14_04_14_59/webgl_manifest.bin.gz",
         center: [39.52590569999999, -119.80886459999999],
-        maxBounds: [39.52590569999999, -119.80886459999999],
+        //maxBounds: [39.52590569999999, -119.80886459999999],
         zoom: 18,
         indoorsEnabled: true,
         tap: true
       });
-
+      map.locate({setView: true});
       //set map boundaries so cant scroll away from museum **NOT WORKING**
-      var corner1 = L.latLng(39.22290569999999, -119.50486459999999),
-      corner2 = L.latLng(39.82990569999999, -119.91986459999999),
-      bounds = L.latLngBounds(corner1, corner2);
-      map.setMaxBounds(bounds);
+      //var corner1 = L.latLng(39.22290569999999, -119.50486459999999),
+      // corner2 = L.latLng(39.82990569999999, -119.91986459999999),
+      //bounds = L.latLngBounds(corner1, corner2);
+      //map.setMaxBounds(bounds);
 
       //map = L.map('map', {doubleClickZoom: false}).locate({setView: true, maxZoom: 16, tap: true, watch: true, enableHighAccuracy: true, fitBounds: [39.52590569999999, -119.80886459999999]});
 
@@ -44,40 +46,21 @@ export class MapPage {
       map.openPopup("Touch Me!", [39.52590569999999, -119.80886459999999], { elevation: 10.0 });
 
       //Sample highlighted area of museum 
-      var circle = L.circle([39.52578148753415, -119.80921509900838], {
+      L.circle([39.52578148753415, -119.80921509900838], {
       radius: 3.0,
       indoorMapId: "EIM-ad6cd210-0145-4881-878e-8f5455693527",
       indoorMapFloorId: 0
       }).addTo(map);
 
-
-    //Track User Location **NOT WORKING**
-      function onLocationFound(e) {
-        var radius = e.accuracy / 2;
-
-        L.marker(e.latlng).addTo(map)
-        .bindPopup("You are within " + radius + " meters from this point").openPopup();
-
-        L.circle(e.latlng, radius).addTo(map);
-    }
-
-    function onLocationError(e) {
-        alert(e.message);
-    }
-
-    map.on('locationfound', onLocationFound);
-    map.on('locationerror', onLocationError);
-
-    map.locate({setView: true, maxZoom: 16});
       //precache map to load faster and work offline
-      var cacheCompleteCallback = function(success) {
-        if (success) {
-          console.log("Caching complete");
-        } else {
-          console.log("Caching failed");
-        }
-      }
-      map.precache([39.62590569999999, -119.90886459999999], 2000, cacheCompleteCallback);
+      //var cacheCompleteCallback = function(success) {
+        //if (success) {
+          //console.log("Caching complete");
+        //} else {
+         // console.log("Caching failed");
+        //}
+      //}
+      //map.precache([39.62590569999999, -119.90886459999999], 2000, cacheCompleteCallback);
 
       //debug for poi points to latLng
       map.on('click', function(e) {
@@ -97,10 +80,20 @@ export class MapPage {
       map.indoors.on("indoormapexit", onExit)
 
 
+      L.control.locate({
+         position: 'topleft',
+         setView: 'always',
+         drawMarker: true,
+         drawCircle: true,
+         strings: {
+         title: "Show me where I am!"
+         },
+         locateOptions: {
+               enableHighAccuracy: true
+         }
+      }).addTo(map);
 
-
-
-
+      
   }
 
 
