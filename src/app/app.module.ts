507fa633d93cfb83bcde6e744de756ca97ba237f
Sahana Acharya <sahana.acharya15@gmail.com>
2018-05-01 07:38:23 -0700

    updated app components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,10 +17,13 @@ import { QuizEndPage } from '../pages/quiz-end/quiz-end';
 import { PersonalityPage } from '../pages/personality/personality';
 import { MapPage } from '../pages/map/map';
 import { DataProvider } from '../providers/data/data';
+import { DataProvider2 } from '../providers/data/data1';
 import { FlashCardComponent } from '../components/flash-card/flash-card';
 import { MissionsPage } from '../pages/missions/missions';
 import { LoginDataProvider } from '../providers/login-data/login-data';
+import { Missions2Page } from '../pages/missions/missions2';
 import { LeaderboardPage } from '../pages/leaderboard/leaderboard';
+import { ProgressBarComponent } from '../components/progress-bar/progress-bar';
 
 @NgModule({
   declarations: [
@@ -34,6 +37,8 @@ import { LeaderboardPage } from '../pages/leaderboard/leaderboard';
     MapPage,
     FlashCardComponent,
     MissionsPage,
+    Missions2Page,
+    ProgressBarComponent,
     LeaderboardPage
   ],
   imports: [
@@ -53,6 +58,7 @@ import { LeaderboardPage } from '../pages/leaderboard/leaderboard';
     PersonalityPage,
     MapPage,
     MissionsPage,
+    Missions2Page,
     LeaderboardPage
   ],
   providers: [
@@ -60,6 +66,7 @@ import { LeaderboardPage } from '../pages/leaderboard/leaderboard';
     SplashScreen,
     {provide: ErrorHandler, useClass: IonicErrorHandler},
     DataProvider,
+    DataProvider2,
     LoginDataProvider
   ]
 })
